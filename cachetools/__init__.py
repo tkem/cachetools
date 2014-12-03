a4f3125a837ebbcb1cc1e99351db0e1b1b7e6445
Thomas Kemmer <tkemmer@computer.org>
2014-12-03 12:16:47 +0100

    Rename cache implementation source files.

--- cachetools/__init__.py
+++ cachetools/__init__.py
@@ -1,17 +1,17 @@
 """Extensible memoizing collections and decorators"""
 
 from .cache import Cache
-from .rrcache import RRCache, rr_cache
-from .lfucache import LFUCache, lfu_cache
-from .lrucache import LRUCache, lru_cache
-from .ttlcache import TTLCache, ttl_cache
 from .decorators import cachedmethod
+from .lfu import LFUCache, lfu_cache
+from .lru import LRUCache, lru_cache
+from .rr import RRCache, rr_cache
+from .ttl import TTLCache, ttl_cache
 
 __all__ = (
     'Cache',
-    'RRCache', 'LFUCache', 'LRUCache', 'TTLCache',
-    'rr_cache', 'lfu_cache', 'lru_cache', 'ttl_cache',
     'cachedmethod'
+    'LFUCache', 'LRUCache', 'RRCache', 'TTLCache',
+    'lfu_cache', 'lru_cache', 'rr_cache', 'ttl_cache',
 )
 
 __version__ = '0.8.0alpha'
