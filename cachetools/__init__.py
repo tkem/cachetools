00f1aef41d96b32d0bac1d8afcfd69f030c046c0
Thomas Kemmer <tkemmer@computer.org>
2015-08-26 18:02:48 +0200

    Fix #37: Add key argument to @cachedmethod.

--- cachetools/__init__.py
+++ cachetools/__init__.py
@@ -1,6 +1,7 @@
 """Extensible memoizing collections and decorators"""
 
 import functools
+import warnings
 
 from .base import Cache
 from .func import lfu_cache, lru_cache, rr_cache, ttl_cache
@@ -18,6 +19,8 @@ __all__ = (
 )
 
 __version__ = '1.0.3'
+
+_default = []  # evaluates to False
 
 
 def cache(cache, key=hashkey, lock=None):
@@ -64,31 +67,65 @@ def cache(cache, key=hashkey, lock=None):
     return decorator
 
 
-def cachedmethod(cache, typed=False):
+def cachedmethod(cache, key=_default, lock=None, typed=_default):
     """Decorator to wrap a class or instance method with a memoizing
     callable that saves results in a (possibly shared) cache.
 
     """
-    key = typedkey if typed else hashkey
+    if key is not _default and not callable(key):
+        key, typed = _default, key
+    if typed is not _default:
+        warnings.warn("Passing 'typed' to cachedmethod() is deprecated, "
+                      "use 'key=typedkey' instead", DeprecationWarning, 2)
 
     def decorator(method):
-        def wrapper(self, *args, **kwargs):
-            c = cache(self)
-            if c is None:
-                return method(self, *args, **kwargs)
-            k = key(method, *args, **kwargs)
-            try:
-                return c[k]
-            except KeyError:
-                pass  # key not found
-            v = method(self, *args, **kwargs)
-            try:
-                c[k] = v
-            except ValueError:
-                pass  # value too large
-            return v
+        # pass method to default key function for backwards compatibilty
+        if key is _default:
+            makekey = functools.partial(typedkey if typed else hashkey, method)
+        else:
+            makekey = key  # custom key function always receive method args
+        if lock is None:
+            def wrapper(self, *args, **kwargs):
+                c = cache(self)
+                if c is None:
+                    return method(self, *args, **kwargs)
+                k = makekey(self, *args, **kwargs)
+                try:
+                    return c[k]
+                except KeyError:
+                    pass  # key not found
+                v = method(self, *args, **kwargs)
+                try:
+                    c[k] = v
+                except ValueError:
+                    pass  # value too large
+                return v
+        else:
+            def wrapper(self, *args, **kwargs):
+                c = cache(self)
+                if c is None:
+                    return method(self, *args, **kwargs)
+                k = makekey(self, *args, **kwargs)
+                try:
+                    with lock:
+                        return c[k]
+                except KeyError:
+                    pass  # key not found
+                v = method(self, *args, **kwargs)
+                try:
+                    with lock:
+                        c[k] = v
+                except ValueError:
+                    pass  # value too large
+                return v
+        functools.update_wrapper(wrapper, method)
+        if not hasattr(wrapper, '__wrapped__'):
+            wrapper.__wrapped__ = method  # Python < 3.2
 
-        wrapper.cache = cache
-        return functools.update_wrapper(wrapper, method)
-
+        def getter(self):
+            warnings.warn('%s.cache is deprecated' % method.__name__,
+                          DeprecationWarning, 2)
+            return cache(self)
+        wrapper.cache = getter
+        return wrapper
     return decorator
