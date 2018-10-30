3471c3524ef51293c573fd4398345910fd51b8fe
hephex <figus.federico@gmail.com>
2018-10-30 08:34:11 +0000

    skip async tests in <3.5

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import os.path
+import sys
+
+
+def pytest_ignore_collect(path):
+    if sys.version_info >= (3, 5):
+        # do not skip any file when running tests with 3.5+
+        return False
+
+    else:
+        # skip async tests when running tests with <3.5
+        return 'async' in os.path.basename(path)
