6682073d7d4098348889f187a6294522f32da098
hephex <figus.federico@gmail.com>
2018-10-30 09:06:00 +0000

    fix python <3.5 tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,4 @@ def pytest_ignore_collect(path):
 
     else:
         # skip async tests when running tests with <3.5
-        return 'async' in os.path.basename(path)
+        return 'async' in os.path.basename(str(path))
