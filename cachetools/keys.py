325c3cd3441928ba00d0da7daca197d9a640b6b9
Florian Wilhelm <florian.wilhelm@inovex.de>
2019-05-18 14:09:25 +0200

    Even simpler, just use 'object' as singleton

--- cachetools/keys.py
+++ cachetools/keys.py
@@ -25,18 +25,7 @@ class _HashedTuple(tuple):
         return {}  # Don't pickle the runtime-dependent hashvalue
 
 
-class _KeyWordMark(object):
-    # defines this class as singleton
-    def __new__(cls, *args, **kwargs):
-        it = cls.__dict__.get("__it__")
-        if it is not None:
-            return it
-        cls.__it__ = it = object.__new__(cls)
-        it.__init__(*args, **kwargs)
-        return it
-
-
-_kwmark = (_KeyWordMark(),)
+_kwmark = (object,)
 
 
 def hashkey(*args, **kwargs):
