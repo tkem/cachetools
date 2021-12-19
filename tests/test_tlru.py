0e778e4410641af906930877c14f12f592d16fe2
Thomas Kemmer <tkemmer@computer.org>
2021-12-19 12:40:17 +0100

    Refactor time aware caches.

--- tests/test_tlru.py
+++ tests/test_tlru.py
@@ -38,65 +38,63 @@ class TLRUCacheTest(unittest.TestCase, CacheTestMixin):
         self.assertEqual(3, cache.ttu(None, 1, 1))
 
         cache[1] = 1
-        self.assertEqual({1}, set(cache))
-        self.assertEqual(1, len(cache))
         self.assertEqual(1, cache[1])
+        self.assertEqual(1, len(cache))
+        self.assertEqual({1}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual({1}, set(cache))
-        self.assertEqual(1, len(cache))
         self.assertEqual(1, cache[1])
+        self.assertEqual(1, len(cache))
+        self.assertEqual({1}, set(cache))
 
         cache[2] = 2
-        self.assertEqual({1, 2}, set(cache))
-        self.assertEqual(2, len(cache))
         self.assertEqual(1, cache[1])
         self.assertEqual(2, cache[2])
+        self.assertEqual(2, len(cache))
+        self.assertEqual({1, 2}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual({2}, set(cache))
-        self.assertEqual(1, len(cache))
         self.assertNotIn(1, cache)
         self.assertEqual(2, cache[2])
+        self.assertEqual(1, len(cache))
+        self.assertEqual({2}, set(cache))
 
         cache[3] = 3
-        self.assertEqual({2, 3}, set(cache))
-        self.assertEqual(2, len(cache))
         self.assertNotIn(1, cache)
         self.assertEqual(2, cache[2])
         self.assertEqual(3, cache[3])
+        self.assertEqual(2, len(cache))
+        self.assertEqual({2, 3}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual({2, 3}, set(cache))
-        self.assertEqual(2, len(cache))
         self.assertNotIn(1, cache)
         self.assertEqual(2, cache[2])
         self.assertEqual(3, cache[3])
+        self.assertEqual(2, len(cache))
+        self.assertEqual({2, 3}, set(cache))
 
         cache[1] = 1
-        self.assertEqual({1, 2, 3}, set(cache))
-        self.assertEqual(3, len(cache))
         self.assertEqual(1, cache[1])
         self.assertEqual(2, cache[2])
         self.assertEqual(3, cache[3])
+        self.assertEqual(3, len(cache))
+        self.assertEqual({1, 2, 3}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual({1, 3}, set(cache))
-        self.assertEqual(2, len(cache))
         self.assertEqual(1, cache[1])
         self.assertNotIn(2, cache)
         self.assertEqual(3, cache[3])
+        self.assertEqual(2, len(cache))
+        self.assertEqual({1, 3}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual({3}, set(cache))
-        self.assertEqual(1, len(cache))
         self.assertNotIn(1, cache)
         self.assertNotIn(2, cache)
         self.assertEqual(3, cache[3])
+        self.assertEqual(1, len(cache))
+        self.assertEqual({3}, set(cache))
 
         cache.timer.tick()
-        self.assertEqual(set(), set(cache))
-        self.assertEqual(0, len(cache))
         self.assertNotIn(1, cache)
         self.assertNotIn(2, cache)
         self.assertNotIn(3, cache)
@@ -107,6 +105,9 @@ class TLRUCacheTest(unittest.TestCase, CacheTestMixin):
             cache.pop(2)
         with self.assertRaises(KeyError):
             del cache[3]
+
+        self.assertEqual(0, len(cache))
+        self.assertEqual(set(), set(cache))
 
     def test_ttu_lru(self):
         cache = TLRUCache(maxsize=2, ttu=lambda k, v, t: t + 1, timer=Timer())
