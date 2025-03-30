fd4ce114d61fda671d25db668b104227c5084a70
Isidro Arias <isidroariass@hotmail.es>
2025-03-30 13:32:37 +0200

    Simplify FIFO
    This allows to more easily spot the differencies with the other variants.
    
    Also, it saves a bit of memory.
    
    Similar to the OrderedDict recipe:
    	https://docs.python.org/3.12/library/collections.html#collections.OrderedDict:~:text=class%20LastUpdatedOrderedDict(OrderedDict)%3A%0A%20%20%20%20%27Store%20items%20in%20the%20order%20the%20keys%20were%20last%20added%27%0A%0A%20%20%20%20def%20__setitem__(self%2C%20key%2C%20value)%3A%0A%20%20%20%20%20%20%20%20super().__setitem__(key%2C%20value)%0A%20%20%20%20%20%20%20%20self.move_to_end(key)

--- src/cachetools/__init__.py
+++ src/cachetools/__init__.py
@@ -147,25 +147,17 @@ class Cache(collections.abc.MutableMapping):
 class FIFOCache(Cache):
     """First In First Out (FIFO) cache implementation."""
 
-    def __init__(self, maxsize, getsizeof=None):
-        Cache.__init__(self, maxsize, getsizeof)
-        self.__order = collections.OrderedDict()
-
     def __setitem__(self, key, value, cache_setitem=Cache.__setitem__):
         cache_setitem(self, key, value)
         try:
-            self.__order.move_to_end(key)
+            self.move_to_end(key)
         except KeyError:
-            self.__order[key] = None
-
-    def __delitem__(self, key, cache_delitem=Cache.__delitem__):
-        cache_delitem(self, key)
-        del self.__order[key]
+            self[key] = None
 
     def popitem(self):
         """Remove and return the `(key, value)` pair first inserted."""
         try:
-            key = next(iter(self.__order))
+            key = next(iter(self))
         except StopIteration:
             raise KeyError("%s is empty" % type(self).__name__) from None
         else:
