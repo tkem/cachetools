b6301013989f1f89ae320df5b2e340dd65723dd5
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-10-01 19:24:36 -0600

    docs: clarify what units the `ttl` argument uses (i.e., seconds)

--- src/cachetools/func.py
+++ src/cachetools/func.py
@@ -92,6 +92,8 @@ def ttl_cache(maxsize=128, ttl=600, timer=time.monotonic, typed=False):
     """Decorator to wrap a function with a memoizing callable that saves
     up to `maxsize` results based on a Least Recently Used (LRU)
     algorithm with a per-item time-to-live (TTL) value.
+
+    By default, the time-to-live is set to 600 seconds (10 minutes).
     """
     if maxsize is None:
         return _cache(_UnboundTTLCache(ttl, timer), None, typed)
