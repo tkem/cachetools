a6afb502f422bd0c9cb47c7920cb334385143aa6
Thomas Kemmer <tkemmer@computer.org>
2025-12-25 20:27:41 +0100

    Initial attept at @cachedmethod info support.

--- src/cachetools/_cachedmethod.py
+++ src/cachetools/_cachedmethod.py
@@ -97,6 +97,150 @@ class DescriptorBase:
         return self.__cache_clear(objtype)
 
 
+def _condition_info(method, cache, key, lock, cond, info):
+    pending = weakref.WeakKeyDictionary()
+
+    def wrapper(self, *args, **kwargs):
+        c = cache(self)
+        k = key(self, *args, **kwargs)
+        with lock(self):
+            p = pending.setdefault(self, set())
+            cond(self).wait_for(lambda: k not in p)
+            try:
+                return c[k]
+            except KeyError:
+                p.add(k)
+        try:
+            v = method(self, *args, **kwargs)
+            with lock(self):
+                try:
+                    c[k] = v
+                except ValueError:
+                    pass  # value too large
+                return v
+        finally:
+            with lock(self):
+                pending[self].remove(k)
+                cond(self).notify_all()
+
+    def cache_clear(self):
+        c = cache(self)
+        with lock(self):
+            c.clear()
+
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key)
+                self.__obj = obj
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self.__obj, *args, **kwargs)
+
+            def cache_clear(self):
+                return cache_clear(self.__obj)
+
+            @property
+            def cache_lock(self):
+                return lock(self.__obj)
+
+            @property
+            def cache_condition(self):
+                return cond(self.__obj)
+
+        # called for @classmethod with Python >= 3.13
+        def __call__(self, *args, **kwargs):
+            warn_classmethod(stacklevel=3)
+            return wrapper(*args, **kwargs)
+
+    return Descriptor()
+
+
+def _locked_info(method, cache, key, lock, info):
+    def wrapper(self, *args, **kwargs):
+        c = cache(self)
+        k = key(self, *args, **kwargs)
+        with lock(self):
+            try:
+                return c[k]
+            except KeyError:
+                pass  # key not found
+        v = method(self, *args, **kwargs)
+        # in case of a race, prefer the item already in the cache
+        with lock(self):
+            try:
+                return c.setdefault(k, v)
+            except ValueError:
+                return v  # value too large
+
+    def cache_clear(self):
+        c = cache(self)
+        with lock(self):
+            c.clear()
+
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key)
+                self.__obj = obj
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self.__obj, *args, **kwargs)
+
+            def cache_clear(self):
+                return cache_clear(self.__obj)
+
+            @property
+            def cache_lock(self):
+                return lock(self.__obj)
+
+        # called for @classmethod with Python >= 3.13
+        def __call__(self, *args, **kwargs):
+            warn_classmethod(stacklevel=3)
+            return wrapper(*args, **kwargs)
+
+    return Descriptor()
+
+
+def _unlocked_info(method, cache, key, info):
+    def wrapper(self, *args, **kwargs):
+        c = cache(self)
+        k = key(self, *args, **kwargs)
+        try:
+            return c[k]
+        except KeyError:
+            pass  # key not found
+        v = method(self, *args, **kwargs)
+        try:
+            c[k] = v
+        except ValueError:
+            pass  # value too large
+        return v
+
+    def cache_clear(self):
+        c = cache(self)
+        c.clear()
+
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key)
+                self.__obj = obj
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self.__obj, *args, **kwargs)
+
+            def cache_clear(self):
+                return cache_clear(self.__obj)
+
+        # called for @classmethod with Python >= 3.13
+        def __call__(self, *args, **kwargs):
+            warn_classmethod(stacklevel=3)
+            return wrapper(*args, **kwargs)
+
+    return Descriptor()
+
+
 def _condition(method, cache, key, lock, cond):
     # backward-compatible weakref dictionary for Python >= 3.13
     pending = weakref.WeakKeyDictionary()
@@ -219,15 +363,25 @@ def _unlocked(method, cache, key):
     return Descriptor(wrapper, cache_clear)
 
 
-def _wrapper(method, cache, key, lock=None, cond=None):
-    if cond is not None and lock is not None:
-        wrapper = _condition(method, cache, key, lock, cond)
-    elif cond is not None:
-        wrapper = _condition(method, cache, key, cond, cond)
-    elif lock is not None:
-        wrapper = _locked(method, cache, key, lock)
+def _wrapper(method, cache, key, lock=None, cond=None, info=None):
+    if info is not None:
+        if cond is not None and lock is not None:
+            wrapper = _condition_info(method, cache, key, lock, cond, info)
+        elif cond is not None:
+            wrapper = _condition_info(method, cache, key, cond, cond, info)
+        elif lock is not None:
+            wrapper = _locked_info(method, cache, key, lock, info)
+        else:
+            wrapper = _unlocked_info(method, cache, key, info)
     else:
-        wrapper = _unlocked(method, cache, key)
+        if cond is not None and lock is not None:
+            wrapper = _condition(method, cache, key, lock, cond)
+        elif cond is not None:
+            wrapper = _condition(method, cache, key, cond, cond)
+        elif lock is not None:
+            wrapper = _locked(method, cache, key, lock)
+        else:
+            wrapper = _unlocked(method, cache, key)
 
     # backward-compatible properties for @classmethod
     wrapper.cache = cache
