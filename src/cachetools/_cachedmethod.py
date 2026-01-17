a04032e8227ff9bef91fffc3669d85aedad45794
Thomas Kemmer <tkemmer@computer.org>
2026-01-17 21:40:32 +0100

    Fix #357: Convert @cachedmethod decorators to descriptors.

--- src/cachetools/_cachedmethod.py
+++ src/cachetools/_cachedmethod.py
@@ -4,19 +4,112 @@ import functools
 import weakref
 
 
+def warn_classmethod(stacklevel):
+    from warnings import warn
+
+    warn(
+        "decorating class methods with @cachedmethod is deprecated",
+        DeprecationWarning,
+        stacklevel=stacklevel,
+    )
+
+
+class WrapperBase:
+    def __init__(self, obj, method, cache, key, lock=None, cond=None):
+        if type(obj) is type:
+            warn_classmethod(stacklevel=5)
+        functools.update_wrapper(self, method)
+        self._obj = obj  # protected
+        self.__cache = cache
+        self.__key = key
+        self.__lock = lock
+        self.__cond = cond
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError()  # pragma: no cover
+
+    def cache_clear(self):
+        raise NotImplementedError()  # pragma: no cover
+
+    @property
+    def cache(self):
+        return self.__cache(self._obj)
+
+    @property
+    def cache_key(self):
+        return self.__key  # TODO: how to handle self?
+
+    @property
+    def cache_lock(self):
+        return None if self.__lock is None else self.__lock(self._obj)
+
+    @property
+    def cache_condition(self):
+        return None if self.__cond is None else self.__cond(self._obj)
+
+
+class DescriptorBase:
+    def __init__(self, wrapper, cache_clear):
+        self.__attrname = None
+        self.__wrapper = wrapper
+        self.__cache_clear = cache_clear
+
+    def __set_name__(self, owner, name):
+        if self.__attrname is None:
+            self.__attrname = name
+        elif name != self.__attrname:  # pragma: no cover
+            raise TypeError(
+                "Cannot assign the same @cachedmethod to two different names "
+                f"({self.__attrname!r} and {name!r})."
+            )
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self  # deprecated @classmethod
+        wrapper = self.Wrapper(obj)
+        if self.__attrname is not None:
+            try:
+                wrapper = obj.__dict__.setdefault(self.__attrname, wrapper)
+            except AttributeError:  # pragma: no cover
+                # not all objects have __dict__ (e.g. class defines slots)
+                msg = (
+                    f"No '__dict__' attribute on {type(obj).__name__!r} "
+                    f"instance to cache {self.__attrname!r} property."
+                )
+                raise TypeError(msg) from None
+            except TypeError:  # pragma: no cover
+                msg = (
+                    f"The '__dict__' attribute on {type(obj).__name__!r} "
+                    f"instance does not support item assignment for "
+                    f"caching {self.__attrname!r} property."
+                )
+                raise TypeError(msg) from None
+        return wrapper
+
+    # called for @classmethod with Python >= 3.13
+    def __call__(self, *args, **kwargs):
+        warn_classmethod(stacklevel=3)
+        return self.__wrapper(*args, **kwargs)
+
+    # backward-compatible @classmethod handling with Python >= 3.13
+    def cache_clear(self, objtype):
+        warn_classmethod(stacklevel=3)
+        return self.__cache_clear(objtype)
+
+
 def _condition(method, cache, key, lock, cond):
+    # backward-compatible weakref dictionary for Python >= 3.13
     pending = weakref.WeakKeyDictionary()
 
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self, pending, *args, **kwargs):
         c = cache(self)
         k = key(self, *args, **kwargs)
         with lock(self):
-            p = pending.setdefault(self, set())
-            cond(self).wait_for(lambda: k not in p)
+            cond(self).wait_for(lambda: k not in pending)
             try:
                 return c[k]
             except KeyError:
-                p.add(k)
+                pending.add(k)
         try:
             v = method(self, *args, **kwargs)
             with lock(self):
@@ -27,7 +120,7 @@ def _condition(method, cache, key, lock, cond):
                 return v
         finally:
             with lock(self):
-                pending[self].remove(k)
+                pending.remove(k)
                 cond(self).notify_all()
 
     def cache_clear(self):
@@ -35,8 +128,24 @@ def _condition(method, cache, key, lock, cond):
         with lock(self):
             c.clear()
 
-    wrapper.cache_clear = cache_clear
-    return wrapper
+    def classmethod_wrapper(self, *args, **kwargs):
+        p = pending.setdefault(self, set())
+        return wrapper(self, p, *args, **kwargs)
+
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key, lock, cond)
+                self.__pending = set()
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self._obj, self.__pending, *args, **kwargs)
+
+            # objtype: backward-compatible @classmethod handling with Python < 3.13
+            def cache_clear(self, _objtype=None):
+                return cache_clear(self._obj)
+
+    return Descriptor(classmethod_wrapper, cache_clear)
 
 
 def _locked(method, cache, key, lock):
@@ -61,8 +170,19 @@ def _locked(method, cache, key, lock):
         with lock(self):
             c.clear()
 
-    wrapper.cache_clear = cache_clear
-    return wrapper
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key, lock)
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self._obj, *args, **kwargs)
+
+            # objtype: backward-compatible @classmethod handling with Python < 3.13
+            def cache_clear(self, _objtype=None):
+                return cache_clear(self._obj)
+
+    return Descriptor(wrapper, cache_clear)
 
 
 def _unlocked(method, cache, key):
@@ -84,8 +204,19 @@ def _unlocked(method, cache, key):
         c = cache(self)
         c.clear()
 
-    wrapper.cache_clear = cache_clear
-    return wrapper
+    class Descriptor(DescriptorBase):
+        class Wrapper(WrapperBase):
+            def __init__(self, obj):
+                super().__init__(obj, method, cache, key)
+
+            def __call__(self, *args, **kwargs):
+                return wrapper(self._obj, *args, **kwargs)
+
+            # objtype: backward-compatible @classmethod handling with Python < 3.13
+            def cache_clear(self, _objtype=None):
+                return cache_clear(self._obj)
+
+    return Descriptor(wrapper, cache_clear)
 
 
 def _wrapper(method, cache, key, lock=None, cond=None):
@@ -98,6 +229,7 @@ def _wrapper(method, cache, key, lock=None, cond=None):
     else:
         wrapper = _unlocked(method, cache, key)
 
+    # backward-compatible properties for @classmethod
     wrapper.cache = cache
     wrapper.cache_key = key
     wrapper.cache_lock = lock if lock is not None else cond
--- tests/test_cachedmethod.py
+++ tests/test_cachedmethod.py
@@ -4,18 +4,16 @@ import warnings
 
 from cachetools import LRUCache, cachedmethod, keys
 
+from . import CountedCondition, CountedLock
+
 
 class Cached:
-
-    class_cache = LRUCache(2)
-    class_count = 0
-
     def __init__(self, cache, count=0):
         self.cache = cache
         self.count = count
 
     @cachedmethod(lambda self: self.cache)
-    def get(self, value):
+    def get(self, value, *args):
         self.count += 1
         return self.count
 
@@ -24,58 +22,38 @@ class Cached:
         self.count += 1
         return self.count
 
-    @classmethod
-    @cachedmethod(lambda cls: cls.class_cache)
-    def get_classmethod(cls, value):
-        cls.class_count += 1
-        return cls.class_count
-
-    @classmethod
-    @cachedmethod(lambda cls: cls.class_cache, key=keys.typedmethodkey)
-    def get_typedclassmethod(cls, value):
-        cls.class_count += 1
-        return cls.class_count
-
 
 class Locked:
     def __init__(self, cache):
         self.cache = cache
         self.count = 0
-        self.lock_count = 0
+        self.lock = CountedLock()
 
-    @cachedmethod(lambda self: self.cache, lock=lambda self: self)
+    @cachedmethod(lambda self: self.cache, lock=lambda self: self.lock)
     def get(self, value):
         self.count += 1
         return self.count
 
-    def __enter__(self):
-        self.lock_count += 1
 
-    def __exit__(self, *exc):
-        pass
-
-
-class Conditioned(Locked):
+class Conditioned:
     def __init__(self, cache):
-        Locked.__init__(self, cache)
-        self.wait_count = 0
-        self.notify_count = 0
+        self.cache = cache
+        self.count = 0
+        self.lock = self.cond = CountedCondition()
 
-    @cachedmethod(lambda self: self.cache, condition=lambda self: self)
+    @cachedmethod(lambda self: self.cache, condition=lambda self: self.cond)
     def get(self, value):
-        return Locked.get.__wrapped__(self, value)
+        self.count += 1
+        return self.count
 
     @cachedmethod(
-        lambda self: self.cache, lock=lambda self: self, condition=lambda self: self
+        lambda self: self.cache,
+        lock=lambda self: self.lock,
+        condition=lambda self: self.cond,
     )
-    def get_lock(self, value):
-        return Locked.get.__wrapped__(self, value)
-
-    def wait_for(self, predicate):
-        self.wait_count += 1
-
-    def notify_all(self):
-        self.notify_count += 1
+    def get_with_lock(self, value):
+        self.count += 1
+        return self.count
 
 
 class Unhashable:
@@ -185,87 +163,87 @@ class CachedMethodTest(unittest.TestCase):
         cached = Locked({})
 
         self.assertEqual(cached.get(0), 1)
-        self.assertEqual(cached.lock_count, 2)
+        self.assertEqual(cached.lock.count, 2)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 4)
+        self.assertEqual(cached.lock.count, 4)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 5)
+        self.assertEqual(cached.lock.count, 5)
         self.assertEqual(cached.get(1.0), 2)
-        self.assertEqual(cached.lock_count, 6)
+        self.assertEqual(cached.lock.count, 6)
         self.assertEqual(cached.get(1.0), 2)
-        self.assertEqual(cached.lock_count, 7)
+        self.assertEqual(cached.lock.count, 7)
 
         cached.cache.clear()
         self.assertEqual(cached.get(1), 3)
-        self.assertEqual(cached.lock_count, 9)
+        self.assertEqual(cached.lock.count, 9)
 
     def test_locked_nospace(self):
         cached = Locked(LRUCache(maxsize=0))
 
         self.assertEqual(cached.get(0), 1)
-        self.assertEqual(cached.lock_count, 2)
+        self.assertEqual(cached.lock.count, 2)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 4)
+        self.assertEqual(cached.lock.count, 4)
         self.assertEqual(cached.get(1), 3)
-        self.assertEqual(cached.lock_count, 6)
+        self.assertEqual(cached.lock.count, 6)
         self.assertEqual(cached.get(1.0), 4)
-        self.assertEqual(cached.lock_count, 8)
+        self.assertEqual(cached.lock.count, 8)
         self.assertEqual(cached.get(1.0), 5)
-        self.assertEqual(cached.lock_count, 10)
+        self.assertEqual(cached.lock.count, 10)
 
     def test_condition_dict(self):
         cached = Conditioned({})
 
         self.assertEqual(cached.get(0), 1)
-        self.assertEqual(cached.lock_count, 3)
-        self.assertEqual(cached.wait_count, 1)
-        self.assertEqual(cached.notify_count, 1)
+        self.assertEqual(cached.lock.count, 3)
+        self.assertEqual(cached.cond.wait_count, 1)
+        self.assertEqual(cached.cond.notify_count, 1)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 6)
-        self.assertEqual(cached.wait_count, 2)
-        self.assertEqual(cached.notify_count, 2)
+        self.assertEqual(cached.lock.count, 6)
+        self.assertEqual(cached.cond.wait_count, 2)
+        self.assertEqual(cached.cond.notify_count, 2)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 7)
-        self.assertEqual(cached.wait_count, 3)
-        self.assertEqual(cached.notify_count, 2)
+        self.assertEqual(cached.lock.count, 7)
+        self.assertEqual(cached.cond.wait_count, 3)
+        self.assertEqual(cached.cond.notify_count, 2)
         self.assertEqual(cached.get(1.0), 2)
-        self.assertEqual(cached.lock_count, 8)
-        self.assertEqual(cached.wait_count, 4)
-        self.assertEqual(cached.notify_count, 2)
+        self.assertEqual(cached.lock.count, 8)
+        self.assertEqual(cached.cond.wait_count, 4)
+        self.assertEqual(cached.cond.notify_count, 2)
         self.assertEqual(cached.get(1.0), 2)
-        self.assertEqual(cached.lock_count, 9)
-        self.assertEqual(cached.wait_count, 5)
-        self.assertEqual(cached.notify_count, 2)
+        self.assertEqual(cached.lock.count, 9)
+        self.assertEqual(cached.cond.wait_count, 5)
+        self.assertEqual(cached.cond.notify_count, 2)
 
         cached.cache.clear()
         self.assertEqual(cached.get(1), 3)
-        self.assertEqual(cached.lock_count, 12)
-        self.assertEqual(cached.wait_count, 6)
-        self.assertEqual(cached.notify_count, 3)
+        self.assertEqual(cached.lock.count, 12)
+        self.assertEqual(cached.cond.wait_count, 6)
+        self.assertEqual(cached.cond.notify_count, 3)
 
     def test_condition_nospace(self):
         cached = Conditioned(LRUCache(maxsize=0))
 
         self.assertEqual(cached.get(0), 1)
-        self.assertEqual(cached.lock_count, 3)
-        self.assertEqual(cached.wait_count, 1)
-        self.assertEqual(cached.notify_count, 1)
+        self.assertEqual(cached.lock.count, 3)
+        self.assertEqual(cached.cond.wait_count, 1)
+        self.assertEqual(cached.cond.notify_count, 1)
         self.assertEqual(cached.get(1), 2)
-        self.assertEqual(cached.lock_count, 6)
-        self.assertEqual(cached.wait_count, 2)
-        self.assertEqual(cached.notify_count, 2)
+        self.assertEqual(cached.lock.count, 6)
+        self.assertEqual(cached.cond.wait_count, 2)
+        self.assertEqual(cached.cond.notify_count, 2)
         self.assertEqual(cached.get(1), 3)
-        self.assertEqual(cached.lock_count, 9)
-        self.assertEqual(cached.wait_count, 3)
-        self.assertEqual(cached.notify_count, 3)
+        self.assertEqual(cached.lock.count, 9)
+        self.assertEqual(cached.cond.wait_count, 3)
+        self.assertEqual(cached.cond.notify_count, 3)
         self.assertEqual(cached.get(1.0), 4)
-        self.assertEqual(cached.lock_count, 12)
-        self.assertEqual(cached.wait_count, 4)
-        self.assertEqual(cached.notify_count, 4)
+        self.assertEqual(cached.lock.count, 12)
+        self.assertEqual(cached.cond.wait_count, 4)
+        self.assertEqual(cached.cond.notify_count, 4)
         self.assertEqual(cached.get(1.0), 5)
-        self.assertEqual(cached.lock_count, 15)
-        self.assertEqual(cached.wait_count, 5)
-        self.assertEqual(cached.notify_count, 5)
+        self.assertEqual(cached.lock.count, 15)
+        self.assertEqual(cached.cond.wait_count, 5)
+        self.assertEqual(cached.cond.notify_count, 5)
 
     def test_unhashable(self):
         cached = Unhashable(LRUCache(maxsize=0))
@@ -281,7 +259,7 @@ class CachedMethodTest(unittest.TestCase):
         cached = Cached(cache)
 
         self.assertEqual(len(cache), 0)
-        self.assertEqual(Cached.get.__wrapped__(cached, 0), 1)
+        self.assertEqual(cached.get.__wrapped__(cached, 0), 1)
         self.assertEqual(len(cache), 0)
         self.assertEqual(cached.get(0), 2)
         self.assertEqual(len(cache), 1)
@@ -292,28 +270,28 @@ class CachedMethodTest(unittest.TestCase):
         cache = {}
         cached = Cached(cache)
 
-        self.assertIs(Cached.get.cache(cached), cache)
-        self.assertIs(Cached.get.cache_key, keys.methodkey)
-        self.assertIs(Cached.get.cache_lock, None)
-        self.assertIs(Cached.get.cache_condition, None)
+        self.assertIs(cached.get.cache, cache)
+        self.assertIs(cached.get.cache_key, keys.methodkey)
+        self.assertIs(cached.get.cache_lock, None)
+        self.assertIs(cached.get.cache_condition, None)
 
     def test_attributes_lock(self):
         cache = {}
         cached = Locked(cache)
 
-        self.assertIs(Locked.get.cache(cached), cache)
-        self.assertIs(Locked.get.cache_key, keys.methodkey)
-        self.assertIs(Locked.get.cache_lock(cached), cached)
-        self.assertIs(Locked.get.cache_condition, None)
+        self.assertIs(cached.get.cache, cache)
+        self.assertIs(cached.get.cache_key, keys.methodkey)
+        self.assertIs(cached.get.cache_lock, cached.lock)
+        self.assertIs(cached.get.cache_condition, None)
 
     def test_attributes_cond(self):
         cache = {}
         cached = Conditioned(cache)
 
-        self.assertIs(Conditioned.get.cache(cached), cache)
-        self.assertIs(Conditioned.get.cache_key, keys.methodkey)
-        self.assertIs(Conditioned.get.cache_lock(cached), cached)
-        self.assertIs(Conditioned.get.cache_condition(cached), cached)
+        self.assertIs(cached.get.cache, cache)
+        self.assertIs(cached.get.cache_key, keys.methodkey)
+        self.assertIs(cached.get.cache_lock, cached.lock)
+        self.assertIs(cached.get.cache_condition, cached.cond)
 
     def test_clear(self):
         cache = {}
@@ -321,7 +299,7 @@ class CachedMethodTest(unittest.TestCase):
 
         self.assertEqual(cached.get(0), 1)
         self.assertEqual(len(cache), 1)
-        Cached.get.cache_clear(cached)
+        cached.get.cache_clear()
         self.assertEqual(len(cache), 0)
 
     def test_clear_locked(self):
@@ -330,10 +308,10 @@ class CachedMethodTest(unittest.TestCase):
 
         self.assertEqual(cached.get(0), 1)
         self.assertEqual(len(cache), 1)
-        self.assertEqual(cached.lock_count, 2)
-        Locked.get.cache_clear(cached)
+        self.assertEqual(cached.lock.count, 2)
+        cached.get.cache_clear()
         self.assertEqual(len(cache), 0)
-        self.assertEqual(cached.lock_count, 3)
+        self.assertEqual(cached.lock.count, 3)
 
     def test_clear_condition(self):
         cache = {}
@@ -341,45 +319,7 @@ class CachedMethodTest(unittest.TestCase):
 
         self.assertEqual(cached.get(0), 1)
         self.assertEqual(len(cache), 1)
-        self.assertEqual(cached.lock_count, 3)
-        Conditioned.get.cache_clear(cached)
+        self.assertEqual(cached.lock.count, 3)
+        cached.get.cache_clear()
         self.assertEqual(len(cache), 0)
-        self.assertEqual(cached.lock_count, 4)
-
-
-class CachedClassMethodTest(unittest.TestCase):
-
-    def test(self):
-        Cached.class_cache = LRUCache(2)
-        Cached.class_count = 0
-        cached = Cached(None)
-
-        self.assertEqual(cached.get_classmethod(0), 1)
-        self.assertEqual(Cached.get_classmethod(0), 1)
-        self.assertEqual(cached.get_classmethod(1), 2)
-        self.assertEqual(Cached.get_classmethod(1), 2)
-        self.assertEqual(cached.get_classmethod(1), 2)
-        self.assertEqual(Cached.get_classmethod(1), 2)
-        self.assertEqual(cached.get_classmethod(1.0), 2)
-        self.assertEqual(Cached.get_classmethod(1.0), 2)
-        self.assertEqual(Cached.get_classmethod(1.1), 3)
-        self.assertEqual(cached.get_classmethod(1.1), 3)
-
-        cached.class_cache.clear()
-        self.assertEqual(cached.get_classmethod(1), 4)
-
-    def test_typedmethod(self):
-        Cached.class_cache = LRUCache(2)
-        Cached.class_count = 0
-        cached = Cached(None)
-
-        self.assertEqual(cached.get_typedclassmethod(0), 1)
-        self.assertEqual(Cached.get_typedclassmethod(0), 1)
-        self.assertEqual(cached.get_typedclassmethod(1), 2)
-        self.assertEqual(Cached.get_typedclassmethod(1), 2)
-        self.assertEqual(cached.get_typedclassmethod(1.0), 3)
-        self.assertEqual(Cached.get_typedclassmethod(1.0), 3)
-        self.assertEqual(cached.get_typedclassmethod(0.0), 4)
-        self.assertEqual(Cached.get_typedclassmethod(0.0), 4)
-        self.assertEqual(Cached.get_typedclassmethod(0), 5)
-        self.assertEqual(cached.get_typedclassmethod(0), 5)
+        self.assertEqual(cached.lock.count, 4)
