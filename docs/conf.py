578e97648dd312880f8aea172437c6dd424d0028
Thomas Kemmer <tkemmer@computer.org>
2026-05-23 21:48:10 +0200

    Update build environment.

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,10 @@
-import pathlib
-import sys
-
-src_directory = (pathlib.Path(__file__).parent.parent / "src").resolve()
-sys.path.insert(0, str(src_directory))
-
-
-# Extract the current version from the source.
-def get_version():
-    """Get the version and release from the source code."""
-
-    text = (src_directory / "cachetools/__init__.py").read_text()
-    for line in text.splitlines():
-        if not line.strip().startswith("__version__"):
-            continue
-        full_version = line.partition("=")[2].strip().strip("\"'")
-        partial_version = ".".join(full_version.split(".")[:2])
-        return full_version, partial_version
-    return (None, None)
+import importlib.metadata
 
 
 project = "cachetools"
 copyright = "2014-2026 Thomas Kemmer"
-release, version = get_version()
+release = importlib.metadata.version(project)
+version = ".".join(release.split(".")[:2])
 
 extensions = [
     "sphinx.ext.autodoc",
